29dbfc9bdb29838582c94f3471eb758563b18db8
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-23 11:54:23 -0300

    Fazendo alterações

--- AnimeLab/anime-lab/src/pages/Home.jsx
+++ AnimeLab/anime-lab/src/pages/Home.jsx
@@ -3,18 +3,6 @@ import "./Home.css";
 import Slider from "react-slick";
 import { AiOutlineStar } from "react-icons/ai";
 
-const settings = {
-  dots: true,
-  fade: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  cssEase: "linear",
-};
-
 const products = [
   {
     name: "Sapato1",
@@ -34,6 +22,24 @@ const products = [
     srcImg:
       "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
   },
+  {
+    name: "Sapato4",
+    price: 35.99,
+    srcImg:
+      "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
+  },
+  {
+    name: "Sapato5",
+    price: 35.99,
+    srcImg:
+      "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
+  },
+  {
+    name: "Sapato6",
+    price: 35.99,
+    srcImg:
+      "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
+  },
 ];
 
 const Home = () => {
@@ -41,19 +47,17 @@ const Home = () => {
     <div className="container-best-sellers">
       <h2>Mais vendidos</h2>
       <div className="products-best-sellers">
-        <Slider {...settings} className="slider">
-          {products.map((product) => {
-            <div className="product" key={product.name}>
-              <img className="img-product" src={product.srcImg} alt="" />
-              <span className="rating">
-                <AiOutlineStar />
-              </span>
-              <span className="name">{product.name}</span>
-              <span className="price">{product.price}</span>
-              <button className="add-car">add car</button>
-            </div>;
-          })}
-        </Slider>
+        {products.map((product) => (
+          <div className="product" key={product.name}>
+            <img className="img-product" src={product.srcImg} alt="" />
+            <span className="rating">
+              <AiOutlineStar />
+            </span>
+            <span className="name">{product.name}</span>
+            <span className="price">{product.price}</span>
+            <button className="add-car">add car</button>
+          </div>
+        ))}
       </div>
     </div>
   );
