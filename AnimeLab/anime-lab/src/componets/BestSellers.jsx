ca179eb850b770cca1902c02a923be18c143b819
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-26 11:11:58 -0300

    Criando um novo componente para os produtos mais vendidos

---
+++ AnimeLab/anime-lab/src/componets/BestSellers.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./BestSellers.css";
+
+const BestSellers = () => {
+  const products = [
+    {
+      name: "Sapato1",
+      price: 35.99,
+      srcImg:
+        "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
+    },
+    {
+      name: "Sapato2",
+      price: 35.99,
+      srcImg:
+        "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
+    },
+    {
+      name: "Sapato3",
+      price: 35.99,
+      srcImg:
+        "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
+    },
+    {
+      name: "Sapato4",
+      price: 35.99,
+      srcImg:
+        "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
+    },
+    {
+      name: "Sapato5",
+      price: 35.99,
+      srcImg:
+        "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
+    },
+    {
+      name: "Sapato6",
+      price: 35.99,
+      srcImg:
+        "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
+    },
+  ];
+
+  return <div></div>;
+};
+
+export default BestSellers;
--- AnimeLab/anime-lab/src/pages/Home.jsx
+++ AnimeLab/anime-lab/src/pages/Home.jsx
@@ -1,66 +1,8 @@
 import React from "react";
 import "./Home.css";
-import Slider from "react-slick";
-import { AiOutlineStar } from "react-icons/ai";
-
-const products = [
-  {
-    name: "Sapato1",
-    price: 35.99,
-    srcImg:
-      "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
-  },
-  {
-    name: "Sapato2",
-    price: 35.99,
-    srcImg:
-      "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
-  },
-  {
-    name: "Sapato3",
-    price: 35.99,
-    srcImg:
-      "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
-  },
-  {
-    name: "Sapato4",
-    price: 35.99,
-    srcImg:
-      "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
-  },
-  {
-    name: "Sapato5",
-    price: 35.99,
-    srcImg:
-      "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
-  },
-  {
-    name: "Sapato6",
-    price: 35.99,
-    srcImg:
-      "https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/helpfull/catalog/darling-in-the-franxx.png",
-  },
-];
 
 const Home = () => {
-  return (
-    <div className="container-best-sellers">
-      <h2>Mais vendidos</h2>
-      <div className="products-best-sellers">
-        {products.map((product) => (
-          <div className="product" key={product.name}>
-            <img className="img-product" src={product.srcImg} alt="" />
-            <span className="rating">
-              <AiOutlineStar />
-            </span>
-            <span className="name">{product.name}</span>
-            <span className="price">{product.price}</span>
-            <button className="add-car">add car</button>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  return <div>home</div>;
 };
 
 export default Home;
